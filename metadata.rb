3122fd4bb2f41fe3bb3ac2119db6135789db85fb
Kazuto Kusama <jacopen@gmail.com>
2014-03-11 21:52:15 +0900

    Change license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'munin'
 maintainer       'Kazuto Kusama'
 maintainer_email 'jacopen@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures munin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
