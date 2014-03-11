1aa7bb9de2f9df78ceb83c9085bad4d16925251d
Kazuto Kusama <jacopen@gmail.com>
2014-03-11 21:52:37 +0900

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jacopen@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures munin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 recipe "munin", "setup master and node"
 recipe "munin::master", "setup master"
